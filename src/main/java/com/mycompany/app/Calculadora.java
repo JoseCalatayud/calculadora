79e2fe173c95a6b6a7924f4c1c565448ac5410c5
jcalata <jcalataruiz@hotmail.com>
2024-11-14 23:07:29 +0100

    metodos y test funcionando V1.0

--- src/main/java/com/mycompany/app/Calculadora.java
+++ src/main/java/com/mycompany/app/Calculadora.java
@@ -7,40 +7,59 @@ public class Calculadora {
         this.resultadoEnMemoria = 0;
     }
 
-    public double sumar(double numero1, double numero2) {
-        return numero1 + numero2;
-    }
-
-    public double sumarAMemoria(double numero) {
-        return numero + this.resultadoEnMemoria;
-    }
-
-    public double restar(double numero1, double numero2) {
-        return numero1 - numero2;
-    }
-
-    public double restarAMemoria( double numero){
-        return this.resultadoEnMemoria - numero;
-    }
-
-    public double dividir ( double numero1, double numero2) throws Exception {
-        return numero1 / numero2;
-    }
-
-    public double dividirAMemoria(double numero){
-        return this.resultadoEnMemoria / numero;
-    }
-
-    public double multiplicar ( double numero1, double numero2){
-        return numero1 * numero2;
-    }
-
-    public double multiplicarAMemoria(double numero){
-        return this.resultadoEnMemoria * numero;
-    }
-
-    public double getResultadoEnMemoria() {
+    public double sumar(double numero1, double numero2) throws IllegalArgumentException {
+        resultadoEnMemoria = numero1 +numero2;
         return resultadoEnMemoria;
+    }
+
+    public double sumar(double numero) throws IllegalArgumentException {
+        resultadoEnMemoria += numero;
+        return resultadoEnMemoria;
+    }
+
+    public double restar(double numero1, double numero2) throws IllegalArgumentException {
+        resultadoEnMemoria = numero1 - numero2;
+        return resultadoEnMemoria;
+    }
+
+    public double restar( double numero) throws IllegalArgumentException{
+        resultadoEnMemoria-=numero;
+        return resultadoEnMemoria;
+    }
+
+    public double dividir ( double numero1, double numero2) throws Exception, IllegalArgumentException {
+        if (numero2 == 0){
+            throw new Exception("El divisor no puede ser 0");
+        }
+        resultadoEnMemoria = numero1 / numero2;
+        return resultadoEnMemoria;
+    }
+
+    public double dividir (double numero) throws Exception , IllegalArgumentException{
+        if(numero==0){
+            throw new Exception("El divisor no puede ser 0");
+        }
+        resultadoEnMemoria /= numero;
+        return resultadoEnMemoria;
+
+    }
+
+    public double multiplicar ( double numero1, double numero2) throws IllegalArgumentException{
+        resultadoEnMemoria = numero1 * numero2;
+        return resultadoEnMemoria;
+    }
+
+    public double multiplicar(double numero) throws IllegalArgumentException{
+        resultadoEnMemoria *= numero;
+        return resultadoEnMemoria;
+    }
+
+    public double getMemoria() {
+        return resultadoEnMemoria;
+    }
+
+    public void borrarMemoria () {
+        resultadoEnMemoria = 0;
     }
 
 }
--- src/test/java/com/mycompany/app/CalculadoraTest.java
+++ src/test/java/com/mycompany/app/CalculadoraTest.java
@@ -1,45 +1,92 @@
 package com.mycompany.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
     @Test
-    void testDividir() {
+    void testDividir() throws Exception {
+        Calculadora cut = new Calculadora();
+        double resultado = cut.dividir(10, 5);
+        assertEquals(2, resultado);
 
     }
 
     @Test
-    void testDividirAMemoria() {
-
+    void testDividirAMemoria() throws Exception {
+        Calculadora cut = new Calculadora();
+        double resultado = cut.dividir(10, 5);
+        resultado = cut.dividir(2);
+        resultado = cut.dividir(2);
+        assertEquals(0.5, resultado);
     }
 
     @Test
     void testMultiplicar() {
-
+        Calculadora cut = new Calculadora();
+        double resultado = cut.multiplicar(10, 5);
+        assertEquals(50, resultado);
     }
 
     @Test
     void testMultiplicarAMemoria() {
-
+        Calculadora cut = new Calculadora();
+        double resultado = cut.multiplicar(10, 5);
+        resultado = cut.multiplicar(2);
+        resultado = cut.multiplicar(3);
+        assertEquals(300, resultado);
     }
 
     @Test
     void testRestar() {
-
+        Calculadora calculadora = new Calculadora();
+        double resultadoSuma = calculadora.restar(34, 5.5);
+        assertEquals(28.5, resultadoSuma);
     }
 
     @Test
     void testRestarAMemoria() {
+        Calculadora calculadora = new Calculadora();
+        double resultadoResta = calculadora.restar(-9);
+        resultadoResta = calculadora.restar(5);
+        resultadoResta = calculadora.restar(5);
+        assertEquals(-1, resultadoResta);
 
     }
 
     @Test
     void testSumar() {
-
+        Calculadora calculadora = new Calculadora();
+        double resultadoSuma = calculadora.sumar(34, 5.5);
+        assertEquals(39.5, resultadoSuma);
     }
 
     @Test
     void testSumarAMemoria() {
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.sumar(8);
+        resultado = calculadora.sumar(22);
+        resultado = calculadora.sumar(22);
+        assertEquals(52, resultado);
+    }
 
+    @Test
+    void testDivisorCero() throws Exception {
+        Calculadora cut = new Calculadora();
+        assertThrows(Exception.class, () -> cut.dividir(10, 0));
+        cut.sumar(1, 5);
+        assertThrows(Exception.class, () -> cut.dividir(0));
+
+    }
+
+    @Test
+    void testBorrarMemoria () {
+        Calculadora cut = new Calculadora();
+        assertEquals(0, cut.getMemoria());
+        cut.sumar(4, 16);
+        cut.multiplicar(3);
+        assertEquals(60, cut.getMemoria());
     }
 }
